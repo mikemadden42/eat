db09a04e0104240ca060803345a241ec9a87dd5a
Michael Madden <mikemadden42@icloud.com>
2017-09-17 14:22:51 -0500

    Clean up with clang-tidy

--- eat.cc
+++ eat.cc
@@ -8,32 +8,33 @@
 using namespace boost::program_options;
 
 int main(int argc, const char *argv[]) {
-  try {
-    options_description desc{"Options"};
-    desc.add_options()("help,h", "Help screen")(
-        "size", value<int>()->default_value(2), "Size");
+    try {
+        options_description desc{"Options"};
+        desc.add_options()("help,h", "Help screen")(
+            "size", value<int>()->default_value(2), "Size");
 
-    variables_map vm;
-    store(parse_command_line(argc, argv, desc), vm);
-    notify(vm);
+        variables_map vm;
+        store(parse_command_line(argc, argv, desc), vm);
+        notify(vm);
 
-    if (vm.count("help"))
-      std::cout << desc << '\n';
-    else if (vm.count("size")) {
-      std::cout << "Allocating " << vm["size"].as<int>() << " GB" << std::endl;
-      const size_t n = vm["size"].as<int>() * 1024;
-      const size_t size = 1024 * 1024;
-      char **array = new char *[n];
-      for (size_t a = 0; a < n; a++) {
-        array[a] = new char[size];
-        memset(array[a], 0, size);
-      }
-      for (size_t a = 0; a < n; a++)
-        delete[] array[a];
-      delete[] array;
-      return 0;
+        if (vm.count("help"))
+            std::cout << desc << '\n';
+        else if (vm.count("size")) {
+            std::cout << "Allocating " << vm["size"].as<int>() << " GB"
+                      << std::endl;
+            const size_t n = vm["size"].as<int>() * 1024;
+            const size_t size = 1024 * 1024;
+            char **array = new char *[n];
+            for (size_t a = 0; a < n; a++) {
+                array[a] = new char[size];
+                memset(array[a], 0, size);
+            }
+            for (size_t a = 0; a < n; a++)
+                delete[] array[a];
+            delete[] array;
+            return 0;
+        }
+    } catch (const error &ex) {
+        std::cerr << ex.what() << '\n';
     }
-  } catch (const error &ex) {
-    std::cerr << ex.what() << '\n';
-  }
 }
